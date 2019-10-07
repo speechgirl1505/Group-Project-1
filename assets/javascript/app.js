d149a1165e2db4b3c3c77eb39177f4bf1d1f7391
Kala Elam <speechgirl1505@gmail.com>
2019-10-07 15:32:16 -0500

    adding js for modals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,63 @@
 function imageScroller() {
-    var images = ["assets/images/bg2 copy.jpeg", "assets/images/fallroad copy.jpg", "assets/images/bryce2 copy.jpeg", "assets/images/tetons copy.jpeg", "assets/images/beardunes copy.jpeg", "assets/images/antelope copy.jpeg", "assets/images/bison copy.jpeg", "assets/images/rocky copy.jpeg", "assets/images/bear copy.jpeg", "assets/images/bryce copy.jpeg", "assets/images/fjords copy.jpeg", "assets/images/geyser copy.jpeg", "assets/images/joshua copy.jpeg", "assets/images/glacier copy.jpeg", "assets/images/moose copy.jpeg", "assets/images/redwoods copy.jpeg", "assets/images/northrim copy.jpeg", "assets/images/silent copy.jpeg", "assets/images/whitesands copy.jpeg", "assets/images/spring copy.jpeg", "assets/images/yosemite copy.jpeg", "assets/images/ysfalls copy.jpeg", "assets/images/zion copy.jpeg"];
-    var index = 0;
-    $("#bg").attr("src", images[index]);
-    
-    setInterval(function () {
-        if (index === images.length - 1) {
-            index = 0;
-            $("#bg").attr("src", images[index]);
-        } else {
-            index++;
-            $("#bg").attr("src", images[index]);
-        }
-    }, 6000);
+  var images = ["assets/images/bg2.jpeg", "assets/images/fallroad.jpg", "assets/images/bryce2.jpeg", "assets/images/tetons.jpeg", "assets/images/beardunes.jpeg", "assets/images/antelope.jpeg", "assets/images/bison.jpeg", "assets/images/rocky.jpeg", "assets/images/bear.jpeg", "assets/images/bryce.jpeg", "assets/images/fjords.jpeg", "assets/images/geyser.jpeg", "assets/images/joshua.jpeg", "assets/images/glacier.jpeg", "assets/images/moose.jpeg", "assets/images/redwoods.jpeg", "assets/images/northrim.jpeg", "assets/images/silent.jpeg", "assets/images/whitesands.jpeg", "assets/images/spring.jpeg", "assets/images/yosemite.jpeg", "assets/images/ysfalls.jpeg", "assets/images/zion.jpeg"];
+  var index = 0;
+  $("#bg").attr("src", images[index]);
+
+  setInterval(function () {
+    if (index === images.length - 1) {
+      index = 0;
+      $("#bg").attr("src", images[index]);
+    } else {
+      index++;
+      $("#bg").attr("src", images[index]);
+    }
+  }, 6000);
 }
 
 
 imageScroller();
 
 $("#searchButton").click(function () {
-    event.preventDefault();
-    $("#searchBar").show();
-    $("#searchButton").hide();
-    
+  event.preventDefault();
+  $("#searchBar").show();
+  $("#searchButton").hide();
+  $("#fillMe").show();
 });
 
-$("#stateButton").click(function(){
-    event.preventDefault();
-    $("#fillMe").hide();
-    $("#searchBar").hide();
-    $("#searchButton").hide();
+$("#stateButton").click(function () {
+  event.preventDefault();
+  $("#fillMe").hide();
+  $("#searchBar").hide();
+  $("#searchButton").hide();
 });
 
 $("#takeMeHome").on("click", function () {
-    event.preventDefault();
-    $("#searchBar").hide();
-    $("#searchButton").show();
-    $("#putItIn").show();
-    $("#fillMe").show();
-    $(".cardsGoHere").empty();
+  event.preventDefault();
+  
+  $("#searchBar").hide();
+  $("#searchButton").show();
+  $("#putItIn").show();
+  $("#fillMe").hide();
+  $(".cardsGoHere").empty();
 });
+
+
 // API
 /*Global Varibales=============================================================================================
-*/ 
+*/
 var parkInfo;
+// var parkCode;
 var imageResponse;
-var stateSelected = "TX";
+var stateSelected = "";
 var unsplashSearch = "";
 var region;
-var beerResponse= "";
+var beerResponse = "";
 
 //Images fixed and working AJAX
 //Unsplash API Below: We are working on having the Unsplash API information incorporate photo based on image and location.
 function unsplashAjaxRequest() {
   var unsplashURL =
-  "https://api.unsplash.com/photos/random/?client_id=b9429332b4931ea777d5218c2dd0c972e59aa521cdd7693c57ae030db53d17ef&query=" + unsplashSearch; //<---here is where our users search generates image related from API
+    "https://api.unsplash.com/photos/random/?client_id=bf539b043528dafae4c292cea01214b50b01cdc58feb7880e0b4964fabcb0a86&query=" + unsplashSearch; //<---here is where our users search generates image related from API
   $.ajax({
     method: "GET",
     url: unsplashURL
@@ -69,6 +73,19 @@ function unsplashAPICall(imgResponse) {
 
 function NPSAjaxRequest() {
 
+
+  // This line grabs the input from the textbox
+  var state = $(".form-control").val().trim();
+  console.log("STATE: " + state);
+  if (state.length > 2) {
+    var stateAcronym = abbrState(state, 'abbr')
+    stateSelected = stateAcronym;
+    console.log("ACRONYM: " + stateAcronym);
+    console.log(stateSelected)
+  }
+  else {
+    stateSelected = state;
+  }
   var queryURL =
     //Hide our key once ready to push! (12:57pm)
     "https://developer.nps.gov/api/v1/parks?stateCode=" + stateSelected + "&api_key=aYUUJ6WlFhXa4FxFlzz8RR712lrylzDsnov99dCe";
@@ -79,7 +96,7 @@ function NPSAjaxRequest() {
 }
 
 function NPSAPICall(response) {
-    console.log(response);
+  console.log(response);
   stateCardGenerator(response);
   // modelGenerator(response)
 }
@@ -95,11 +112,11 @@ function NPSAPICall(response) {
 //   function beerAPICall(beerdbResponse) {
 //     console.log(beerResponse);
 //     beerResponse = beerdbResponse;
-    
+
 //   };
-  
-  function bothAjaxRequests(event) {
-event.preventDefault();
+
+function bothAjaxRequests(event) {
+  event.preventDefault();
 
   unsplashAjaxRequest();
   NPSAjaxRequest();
@@ -107,31 +124,50 @@ event.preventDefault();
 $("#stateButton").on("click", bothAjaxRequests);
 
 
-  // LINDSAY: ON STATE PAGE ======================================================================================
+// HOME PAGE====================================================================================================
+// "take me home" button says take me home
 
-  // When user enters a state into the search bar it hides container containing home page html
+// $('#showModal').on('click', function() {
+//     // jQuery.noConflict();
+//     $('#showModal').modal('show');
+//     });
 
-  // If the state is not a real state name (misspelled, not in the US, etc.,) display error message
-
-  // Populates screen with cards of each national park in the state
-  // MODALS =======================================================================================================
-
-  // On state page with all parks: when user clicks on a card, card pops up a modal about that park
-
-  // Individual park modal: Populates with park information from NPS API, populates with brewery info from brewery API
-
-
-
-
-
-
-
+// $('#exampleModal').on('show.bs.modal', function (event) {
+//     var button = $(event.relatedTarget) // Button that triggered the modal
+//     var recipient = button.data('whatever') // Extract info from data-* attributes
+//     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//     var modal = $(this)
+//     modal.find('.modal-title').text('New message to ' + recipient)
+//     modal.find('.modal-body input').val(recipient)
+//   })
 
 
+// LINDSAY: ON STATE PAGE ======================================================================================
+
+// When user enters a state into the search bar it hides container containing home page html
+
+// If the state is not a real state name (misspelled, not in the US, etc.,) display error message
+
+// Populates screen with cards of each national park in the state
+// MODALS =======================================================================================================
+
+// On state page with all parks: when user clicks on a card, card pops up a modal about that park
+
+// Individual park modal: Populates with park information from NPS API, populates with brewery info from brewery API
 
 
 
 
 
 
-  //END OF CODE
+
+
+
+
+
+
+
+
+
+//END OF CODE
--- assets/javascript/stateCardGenerator.js
+++ assets/javascript/stateCardGenerator.js
@@ -1,42 +1,51 @@
 function stateCardGenerator(response) {
+
+
+
+    // Unsplash search depends on how we are able to get images to load
+
+
+
+
     parkInfo = response;
-    console.log(parkInfo);
+    // console.log(parkInfo);
     // Generating all park names and designations in the state
     var numParks = parkInfo.data.length
     console.log(numParks)
     var stateContainer = $(".cardsGoHere");
     for (i = 0; i < numParks; i++) {
         var parkName = parkInfo.data[i].fullName;
-        userSearch = parkName;
+        unsplashSearch = parkName;
         var parkDesignation = parkInfo.data[i].designation;
+        var parkCode = parkInfo.data[i].parkCode;
         // Image from Unsplash
         var parkImage = imageResponse.urls.full;
-        console.log(parkName);
-        console.log(parkDesignation);
+        // console.log(parkName);
+        // console.log(parkDesignation);
 
-// State Cards Generator
-       // Create new div to hold the card
-       var stateParkCard = $("<div>")
-       // Append card elements
-       stateParkCard.addClass("card card-body")
-       stateParkCard.attr({ "data-toggle": "modal", "data-target": "#exampleModalCenter", "data-value": parkName });
-       var stateImage = $("<img>").attr({ src: parkImage, class: "card-img-top", alt: "..." })
-       stateParkCard.append(stateImage)
-       // var stateParkCardBody = $("<div class='card-body'>")
-       // stateParkCard.append(stateParkCardBody)
-       var stateParkCardHeader = $("<h5 class='card-title'>")
-       stateParkCardHeader.text(parkName)
-       stateParkCard.append(stateParkCardHeader);
-       var stateParkCardP = $("<p class ='card-text'>");
-       stateParkCardP.text(parkDesignation);
-       stateParkCard.append(stateParkCardP);
-       stateContainer.append(stateParkCard);
-
+        // State Cards Generator
+        // Create new div to hold the card
+        var stateParkCard = $("<div>")
+        // Append card elements 
+        stateParkCard.addClass("card card-body")
+        stateParkCard.attr({ "data-toggle": "modal", "data-target": "#exampleModalCenter", "data-value": parkCode });
+        var stateImage = $("<img>").attr({ src: parkImage, class: "card-img-top", alt: "..." })
+        stateParkCard.append(stateImage)
+        // var stateParkCardBody = $("<div class='card-body'>")
+        // stateParkCard.append(stateParkCardBody)
+        var stateParkCardHeader = $("<h5 class='card-title'>")
+        stateParkCardHeader.text(parkName)
+        stateParkCard.append(stateParkCardHeader);
+        var stateParkCardP = $("<p class ='card-text'>");
+        stateParkCardP.text(parkDesignation);
+        stateParkCard.append(stateParkCardP);
+        stateContainer.append(stateParkCard);
     }
     console.log(parkInfo)
-    
+
 
 
 
 };
+
 
